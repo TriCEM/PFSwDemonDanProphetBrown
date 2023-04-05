d801e6d81e46cbddaec3a24717abe7a8b7013590
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-04-05 01:23:03 -0400

    snakemake scripts and cluster items

--- data/SirPent/launch_fomes.sh
+++ data/SirPent/launch_fomes.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
-ROOT=*** # root directory for project (non-scratch)
-WD=*** # scratch directory for networks
+ROOT=/Users/nbrazeau/Documents/Github/PFSwDemonDanProphetBrown/ # root directory for project (non-scratch)
+WD=/Users/nbrazeau/Documents/Github/PFSwDemonDanProphetBrown/data/temp # scratch directory for networks
 NODES=1028 # max number of cluster nodes
 WAIT=30 # number of seconds to wait for files to appear, absorbing some file system latency
 
 snakemake \
-	--snakefile $ROOT/run_fomes.snake \
-	--configfile config_fomes_batch.yaml \
+	--snakefile $ROOT/data/SirPent/run_fomes.snake \
+	--configfile $ROOT/data/SirPent/config_fomes_batch.yaml \
 	--printshellcmds \
 	--directory $WD \
-	--cluster $ROOT/launch.py \
+	--cluster $ROOT/data/SirPent/launch.py \
 	-j $NODES \
 	--rerun-incomplete \
 	--keep-going \
-	--latency-wait $WAIT
+	--latency-wait $WAIT \
+#	--dryrun -p

b0143a0d1f453753f80e298e68fe8f9919c73426
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-04-17 17:23:34 -0400

    update lite

--- data/SirPent/launch_lite.py
+++ data/SirPent/launch_lite.py
@@ -10,8 +10,8 @@ import sys
 from snakemake.utils import read_job_properties
 
 # adjust time and mem PRN
-DEFAULT_PROPERTIES = {	"threads": ("1", "-n {threads}"), "time": ("00:30:00", "-t {time}"),
-			"memory": (4, "--mem={memory}g"), "log_dir": ("jobs/job%j.out", "-o {log_dir}"),
+DEFAULT_PROPERTIES = {	"threads": ("1", "-n {threads}"), "time": ("01:00:00", "-t {time}"),
+			"memory": (8, "--mem={memory}g"), "log_dir": ("jobs/job%j.out", "-o {log_dir}"),
 			"err_dir": ("jobs/job%j.err", "-e {err_dir}") }
 
 os.system("mkdir -p jobs")

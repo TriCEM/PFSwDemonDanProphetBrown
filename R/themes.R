98afe9098f5eb407013b672f41a89c3fba50efe3
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-06-22 17:57:15 -0400

    update themes

--- R/themes.R
+++ R/themes.R
@@ -1,6 +1,6 @@
 library(ggplot2)
-plot_theme <- theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 14),
-                    axis.title = element_text(face = "bold", hjust = 0.5, size = 12),
+plot_theme <- theme(plot.title = element_text(face = "bold", hjust = 0.5, vjust = 0.5, size = 14),
+                    axis.title = element_text(face = "bold", hjust = 0.5, vjust = 0.5, size = 12),
                     axis.text = element_text(hjust = 0.5, size = 11),
                     legend.position = "right",
                     legend.title = element_text(face = "bold", vjust = 0.85, size = 12),
